85dab875e36387b48471907d972c06c7844e23bb
Amund Bjoernstad <amund.bjornstad@outlook.com>
2019-11-29 10:15:54 +0100

    Updates

---
+++ precache-manifest.f41582b4ea226f79335ded2b67d48bee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dd37e49ea63985be7dee8af08bbd1b3",
+    "url": "/react-submodules/index.html"
+  },
+  {
+    "revision": "f181bb1e88f7eb78839a",
+    "url": "/react-submodules/static/css/main.2afe0704.chunk.css"
+  },
+  {
+    "revision": "20b933ec56c52b2479e7",
+    "url": "/react-submodules/static/js/2.e44e5261.chunk.js"
+  },
+  {
+    "revision": "f181bb1e88f7eb78839a",
+    "url": "/react-submodules/static/js/main.ed2ac7a8.chunk.js"
+  },
+  {
+    "revision": "c900d3900270f3cc22a9",
+    "url": "/react-submodules/static/js/runtime-main.6779e2cf.js"
+  }
+]);
