2f854d46b0ead42c6c81774c256a2d4e6a49374e
Amund Bjoernstad <amund.bjornstad@outlook.com>
2019-11-29 09:27:59 +0100

    #update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import './App.css'
-import AboutSection from './Components/AboutSection'
-import Sidebar from './Components/Sidebar'
 import BasicModule from './Components/BasicModule'
 import BasicPropsModule from './Components/BasicPropsModule'
 import DefaultPropsModule from './Components/DefaultPropsModule'
@@ -13,23 +11,18 @@ import Characters from './Components/Characters'
 
 const App = () => {
   return (
-    <div >
-      <main className="App" > {
-
-        /* React modules */
-      } < h5 style={{ padding: 0, margin: 0, marginBottom: 12 + 'px', fontSize: 4 + 'rem', }}>React modules </h5>
-        <section className="modules" >
-          <BasicModule />
-          <BasicPropsModule value='pink' />
-          <DefaultPropsModule />
-          <BasicStateModule />
-          <StateArrayModule />
-          <LocalJsoneModule />
-          <Checklist />
-          <Characters />
-        </section>
-      </main>
-    </div>
+    <main className="App" >
+      <section className="modules" >
+        <BasicModule />
+        <BasicPropsModule value='pink' />
+        <DefaultPropsModule />
+        <BasicStateModule />
+        <StateArrayModule />
+        <LocalJsoneModule />
+        <Checklist />
+        <Characters />
+      </section>
+    </main>
   )
 }
 
